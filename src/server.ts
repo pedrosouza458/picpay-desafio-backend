b8d8ec0d76f7555e5c07d135d06d7257c4edf30e
Pedro Souza <pedro.ads510@gmail.com>
2025-01-31 00:31:51 -0300

    feat: initialize routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
-import Fastify from "fastify"
+import Fastify from "fastify";
+import { TransferRequest } from "./types";
 
-const app = Fastify()
+const app = Fastify();
 
-app.get('/', (request, reply) => {
+app.get("/", (request, reply) => {
     reply.send({
-        message: "Hello world"
-    })
-})
+        message: "Hello world",
+    });
+});
 
-app.listen(({ port: 3000 }), () => {
-    console.log("Server in running in port 3000")
-})
+app.post("/transfer", (request, reply) => {
+    const { value, payer, payee }: TransferRequest = request.body;
+    // TODO find payer id by logged user
+    // TODO find payee by id
+});
+
+app.listen({ port: 3000 }, () => {
+    console.log("Server in running in port 3000");
+});
