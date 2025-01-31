45c7e605249290005df2b2d3bf4d5272c5d9783a
Pedro Souza <pedro.ads510@gmail.com>
2025-01-31 01:24:30 -0300

    feat: implement create-user route

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import Fastify from "fastify";
-import { TransferRequest } from "./types";
+import { CreateUserRequest, TransferRequest } from "./types";
 import { prisma } from "./lib/prisma";
 
 const app = Fastify();
@@ -7,6 +7,35 @@ const app = Fastify();
 app.get("/", (request, reply) => {
     reply.send({
         message: "Hello world",
+    });
+});
+
+app.post("/users", async (request, reply) => {
+    const { fullName, email, password, CNPJ, CPF, role, wallet } =
+        request.body as CreateUserRequest;
+    const createUser = await prisma.user.create({
+        data: {
+            fullName,
+            email,
+            password,
+            CNPJ,
+            CPF,
+            role,
+            wallet,
+        },
+    });
+    if (!createUser) {
+        return reply.status(400).send({
+            message: "Failed to create user",
+        });
+    }
+    if (createUser.role !== "commom" && createUser.role !== "shopkeeper") {
+        return reply.status(200).send({
+            message: "User should have commom or shopkeeper role",
+        });
+    }
+    return reply.status(200).send({
+        message: "User created successfully",
     });
 });
 
