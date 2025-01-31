719144d27da63eb6c8f590029469219282354069
Pedro Souza <pedro.ads510@gmail.com>
2025-01-31 01:08:11 -0300

    chore: add CreateUserRequest interface

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,15 @@
+import { Role } from "./enums";
+
+export interface CreateUserRequest {
+    fullName: string;
+    CPF?: string;
+    CNPJ?: string;
+    email: string;
+    password: string;
+    role: Role;
+    wallet: number;
+}
+
 export interface TransferRequest {
     value: number;
     payer: number;
